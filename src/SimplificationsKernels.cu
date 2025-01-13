5d563cfa992877417fdeb21ee2cfdf6fa2624746
Austin <Austin.W.Snyder@outlook.com>
2025-01-12 20:22:09 -0800

    Ryan and I worked on flattening mins/maxes and the dataset for use in cuda

--- src/SimplificationsKernels.cu
+++ src/SimplificationsKernels.cu
@@ -1,7 +1,20 @@
 extern "C"
 
+
+// Global memory: Slow, can be accessed by all threads regardless of block. Slow!!
+
+// Shared memory: Shared by all threads in the same block, much faster than global memory.
+
+// Local memory: Private to each thread. Used for storing variables that don't fit in registers
+
+
 // This will parallelize the removeUselessAttributes
 __global__ void removeUselessHelper(){
 
+}
 
+__device__ boolean insideHBIntervals(){
+
+
+    return inside;
 }
